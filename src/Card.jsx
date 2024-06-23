93ecc8a1eb56e559d7fb64a503a3ffd54edc35ef
UtkershBasnet <utkershbasnet@gmail.com>
2024-06-23 16:39:47 +0530

    First commit

---
+++ src/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css';
+
+function Card(){
+    return(
+        <>
+            <div className="card">
+                <img alt="my_img" src="https://via.placeholder.com/150"></img>
+                <h2>Utkersh Basnet</h2>
+                <p>I am a Student and play video games</p>
+            </div>
+        </>
+    );
+}
+export default Card;
